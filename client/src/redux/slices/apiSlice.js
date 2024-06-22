5030e60e46b035580d6f953605d54713a51f9000
“wafiyabdullah” <wafiyabdullah39@gmail.com>
2024-06-22 12:14:37 +0800

    fix client and server

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,21 +1,13 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 //api uri for backend
-const API_URI = 'https://collab-family-server.vercel.app/api';
+const API_URI = process.env.NODE_ENV === 'development' 
+  ? 'http://localhost:8800/api' 
+  : 'https://collab-family-server.vercel.app/api';
 
 //base query for api
-const baseQuery = fetchBaseQuery({
-    baseUrl: API_URI,
-    prepareHeaders: (headers, { getState }) => {
-      const token = getState().auth.token;
-      if (token) {
-        headers.set('Authorization', `Bearer ${token}`);
-      }
-      return headers;
-    },
-    credentials: 'include',
-  });
-  
+const baseQuery = fetchBaseQuery({ baseUrl: API_URI });
+
 //api slice for redux
 export const apiSlice = createApi ({
     baseQuery,
