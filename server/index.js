2b401ab8729bef9a556be83f0bd20837da0533b6
“wafiyabdullah” <wafiyabdullah39@gmail.com>
2024-06-25 19:31:24 +0800

    index server

--- server/index.js
+++ server/index.js
@@ -24,9 +24,8 @@ const corsOptions = {
     origin: ["https://collab-family-client.vercel.app", "http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-    allowedHeaders: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
   };
-  
+
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions)); // Explicitly handling preflight requests
 
