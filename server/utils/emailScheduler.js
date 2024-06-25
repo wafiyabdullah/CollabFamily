0ae8710e9dc8daf597273c8848ff7875f4beaea1
“wafiyabdullah” <wafiyabdullah39@gmail.com>
2024-06-26 04:24:06 +0800

    update cron job

--- server/utils/emailScheduler.js
+++ server/utils/emailScheduler.js
@@ -18,7 +18,7 @@ var transporter = nodemailer.createTransport({
 
 async function scheduleEmail() {
     // Schedule email to be sent at 8:29 PM every day
-    cron.schedule('24 4 * * *', async function() { 
+    cron.schedule('25 4 * * *', async function() { 
         
         try{
 
