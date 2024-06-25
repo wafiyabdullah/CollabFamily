48c5948be1bfefe80b53a499e7aac7a5d524e3d3
“wafiyabdullah” <wafiyabdullah39@gmail.com>
2024-06-25 20:37:47 +0800

    fix api slice

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,9 +1,9 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 //api uri for backend
-const API_URI = process.env.NODE_ENV === 'development' 
-  ? 'http://localhost:8800/api' 
-  : 'https://collabfamily.onrender.com/api';
+const API_URI = process.env.NODE_ENV === 'production'
+  ? 'https://collabfamily.onrender.com/api'
+  : 'http://localhost:8800/api';
 
 //base query for api
 const baseQuery = fetchBaseQuery({ baseUrl: API_URI });
